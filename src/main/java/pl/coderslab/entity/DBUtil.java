5ccd152de80b50e8d4b02860d49eca0b089fd5b5
Gosia Jedrzejewska <gorzata.jedrzejewska@gmail.com>
2020-10-02 15:35:34 +0200

    UserDAO

---
+++ src/main/java/pl/coderslab/entity/DBUtil.java
@@ -0,0 +1,17 @@
+package pl.coderslab.entity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private final static String DB_URL =
+            "jdbc:mysql://localhost:3306/workshop2?use_SSL=false&characterEncoding=utf8&serverTimezone=UCT";
+    private final static String DB_USERNAME = "root";
+    private final static String DB_PASSWORD = "coderslab";
+
+    public static Connection connectDB() throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        return connection;
+    }
+}
---
+++ src/main/java/pl/coderslab/entity/Main.java
@@ -0,0 +1,8 @@
+package pl.coderslab.entity;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
